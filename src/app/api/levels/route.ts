1cb47f53c1c9be3c1ed809ed3efe4361f11c11ca
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2024-01-12 00:32:04 -0800

    made a script to auto generate data

--- src/app/api/levels/route.ts
+++ src/app/api/levels/route.ts
@@ -1,6 +1,15 @@
 import prisma from "../../../../prisma/prisma";
 
 export async function GET(request: Request) {
-    let levels = await prisma.level.findMany()
+    let levels = await prisma.level.findMany({
+        include: {
+            packs: true,
+            list: {
+                include: {
+                    player: true,
+                }
+            }
+        }
+    })
     return new Response(JSON.stringify(levels))
 }
