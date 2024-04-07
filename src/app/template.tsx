b8d3858af7a7d812fbaa44912e75a567ce234863
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2024-04-07 12:23:14 -0700

    Update template.tsx

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,5 +1,3 @@
-'use client'
-import Script from 'next/script';
 import { useEffect, useState } from 'react';
 
 export default function RootLayout({
@@ -7,16 +5,10 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-
-    let [load, setLoad] = useState(false)
-    useEffect(() => {
-        setTimeout(() => setLoad(true), 3000)
-    }, [])
-
   return (<>
   {children}
-    {load ? <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4543250064393866"
-     crossOrigin="anonymous"></script> : ""}
+    {/* <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4543250064393866"
+     crossOrigin="anonymous"></script> */}
     </>
   )
 }
