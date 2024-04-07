df75e437a27478c3d74939987de4487ef48924e6
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2024-04-07 13:14:21 -0700

    nice little fix to get the ads rerendered everytime

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,14 +1,32 @@
-'use client';
-export default async function RootLayout({
+'use client'
+import { Router } from "next/router"
+import Script from "next/script"
+import { useEffect } from "react"
+
+export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
 
+    useEffect(() => {
+        let script = document.createElement("script")
+        script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4543250064393866"
+        script.async = true
+        script.crossOrigin = "anonymous"
+
+        document.body.appendChild(script)
+
+        Router.events.on("routeChangeComplete", () => {
+            document.body.removeChild(script)
+            return () => {
+                Router.events.off('routeChangeComplete', () => {})
+            }
+        })
+    }, [Router])
+
   return (<>
 {children}
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4543250064393866"
-     crossOrigin="anonymous"></script>
 </>
   )
 }
