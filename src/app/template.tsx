c45df218107599daa07f360573cfe736bc56fb36
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2024-04-07 11:46:20 -0700

    Update template.tsx

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,12 +1,6 @@
 'use client';
 import './globals.css'
 import '@radix-ui/themes/styles.css';
-import { Theme } from '@radix-ui/themes';
-import Nav from '@/components/Nav';
-import { getServerSession } from 'next-auth';
-import jwt from "jsonwebtoken"
-import { headers } from 'next/headers';
-import Script from 'next/script';
 
 export default function RootTemplate({
   children,
@@ -17,7 +11,7 @@ export default function RootTemplate({
   return (
         <>
             {children}
-            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4543250064393866"
+            <script src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4543250064393866"
      crossOrigin="anonymous" defer></script>
         </>
   )
