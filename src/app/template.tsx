094b92a83adef3ef16a9e465b9582bd37efd6fab
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2024-04-07 11:49:18 -0700

    Update template.tsx

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,4 +1,5 @@
 'use client';
+import Script from 'next/script';
 import './globals.css'
 import '@radix-ui/themes/styles.css';
 
@@ -11,8 +12,8 @@ export default function RootTemplate({
   return (
         <>
             {children}
-            <script src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4543250064393866"
-     crossOrigin="anonymous" defer></script>
+            <Script src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4543250064393866"
+     crossOrigin="anonymous" defer></Script>
         </>
   )
 }
