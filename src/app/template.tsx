32b5630220e1f936431c6aee3040f05e9cff44da
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2024-04-07 13:28:43 -0700

    Update template.tsx

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,35 +1,15 @@
-'use client'
-import { Router } from "next/router"
 import Script from "next/script"
-import { useEffect, useState } from "react"
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
 
-    let [load, setLoad] = useState(true)
-
-    useEffect(() => {
-        Router.events.on("routeChangeStart", () => {
-            setLoad(false)
-            return () => {
-                Router.events.off('routeChangeStart', () => {})
-            }
-        })
-        Router.events.on("routeChangeComplete", () => {
-            setLoad(true)
-            return () => {
-                Router.events.off('routeChangeComplete', () => {})
-            }
-        })
-    }, [Router])
-
   return (<>
 {children}
-{load ? <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4543250064393866"
-     crossOrigin="anonymous"></script> : ""}
+    <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4543250064393866"
+     crossOrigin="anonymous" strategy='lazyOnload'></Script>
 </>
   )
 }
