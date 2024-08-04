f58389c78bff5dfe1fe43e7fabb2125150bb5972
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2024-08-04 00:31:35 -0700

    specify between 10 - 100 profiles per page

--- src/app/leaderboards/client.tsx
+++ src/app/leaderboards/client.tsx
@@ -12,7 +12,7 @@ interface info {
     p: number
 }
 
-export default function LeaderboardClient({profs, p}: info) {
+export default function LeaderboardClient({ profs, p }: info) {
     let [pages, setPages] = useState(p)
     let [profiles, setProfiles] = useState(profs)
     let [nations, setNations] = useState([])
@@ -21,86 +21,109 @@ export default function LeaderboardClient({profs, p}: info) {
     let [loading, setLoading] = useState(false)
     let [filter, setFilter] = useState("")
     let [nationality, setNationality] = useState(["International", "International"])
+    let [max, setMax] = useState(25)
     let queue = ""
+    let sliderQueue = ""
 
-    async function changePage(e: number, f: string = filter, reset: boolean = false, n: string = nationality[1], getNations: typeof type = type) {
+    async function changePage(e: number, f: string = filter, reset: boolean = false, n: string = nationality[1], getNations: typeof type = type, m: number = max) {
         setLoading(true)
-                    let req = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/leaderboards${getNations == "nations" ? '/nations' : ""}?page=${e}${f ? `&name=${encodeURIComponent(f)}` : ""}${n != "International" ?  `&nationality=${n}` : ""}`, {cache: "no-cache"})
-                    let profs = await req.json()
-                    if(getNations == "nations") {
-                        setNations(profs.profiles)
-                    } else {
-                        setProfiles(profs.profiles)
-                    }
-                    setPage(e)
-                    if(reset) setPages(profs.pages)
-                    setLoading(false)
+        let req = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/leaderboards${getNations == "nations" ? '/nations' : ""}?page=${e}${f ? `&name=${encodeURIComponent(f)}` : ""}${n != "International" ? `&nationality=${n}` : ""}&max=${m}`, { cache: "no-cache" })
+        let profs = await req.json()
+        if (getNations == "nations") {
+            setNations(profs.profiles)
+        } else {
+            setProfiles(profs.profiles)
+        }
+        setPage(e)
+        if (reset) setPages(profs.pages)
+        setLoading(false)
     }
 
-  return (
-    <Box style={{opacity: loading ? 0.5 : 1, overflowX: "hidden"}}>
-      <Flex gap="4" style={{placeItems: "center", justifyContent: "center"}}>
-      <img src="/favicon.ico" height="70px"></img>
-        <Text size="9" className="header" style={{display: "contents"}}>Leaderboards</Text>
-        <img src="/favicon.ico" height="70px"></img>
-      </Flex>
-      <br></br>
-      <Text size="5" className="header">This part of the list shows the best {type == "users" ? "players" : "countries"} on the Insane Demon List!</Text>
-        <br></br><br></br>
-        <Grid style={{placeItems: "center"}}>
+    return (
+        <Box style={{ opacity: loading ? 0.5 : 1, overflowX: "hidden" }}>
+            <Flex gap="4" style={{ placeItems: "center", justifyContent: "center" }}>
+                <img src="/favicon.ico" height="70px"></img>
+                <Text size="9" className="header" style={{ display: "contents" }}>Leaderboards</Text>
+                <img src="/favicon.ico" height="70px"></img>
+            </Flex>
             <br></br>
-        <TextFieldRoot style={{width: "min(100%, 800px)"}}>
-            <TextFieldSlot>
-                <MagnifyingGlassIcon style={{scale: 1.8, padding: "5px"}}></MagnifyingGlassIcon>
-            </TextFieldSlot>
-            <TextFieldInput style={{fontSize: "20px", height: "40px"}} placeholder='Search...' onChange={async (e) => {
-                let key = crypto.randomUUID()
-                queue = key
-                setTimeout(() => {
-                    if(queue != key) return;
-                    setFilter(e.target.value)
-                    changePage(1, e.target.value, true)
-                }, 1000)
-            }}></TextFieldInput>
-        </TextFieldRoot>
-        </Grid>
-        <br></br>
-        <Grid style={{placeItems: "center"}}>
-            <SelectRoot defaultValue={JSON.stringify(nationality)} onValueChange={(e) => {
-                let value = JSON.parse(e)
-                 setNationality(value)
-                 changePage(1, undefined, true, value[1])
-            }}>
-                <SelectTrigger style={{width: "min(100%, 400px)", fontSize: "20px"}} color="cyan"></SelectTrigger>
-                <SelectContent>
-                    {Object.entries(cache.nationalities).map(e => <SelectItem key={e[1]} value={JSON.stringify(e)}>
-                    <Flex gap="3">
-            <Image alt={e[1]} src={e[0] != "International" ? `https://raw.githubusercontent.com/lipis/flag-icons/4f420bdd2e954f6da11220f1136fa181ed7019e7/flags/4x3/${e[1]}.svg` : "/international.png"} width={24} height={18}></Image>
-            <Text size="3">{e[0]}</Text>
-        </Flex>
-                    </SelectItem>)}
-                </SelectContent>
-            </SelectRoot>
-        </Grid>
-        <br></br>
-        <Grid style={{placeItems: "center"}}>
-                    <Button color="cyan" style={{width: "min(100%, 350px)"}} onClick={async () => {
-                                let t: typeof type = type == "nations" ? "users" : 'nations'
-                                setType(t)
-                                changePage(1, undefined, true, undefined, t)
-                            }}>
-                            <Text size="5" as="p" align='center'>Switch to {type == "users" ? "nationalities" : "players"} leaderboard</Text>
-                    </Button>
-                    <br></br>
-                    <Pagination
-                page={page}
-                count={pages}
-                onChange={changePage}
-            ></Pagination>
-            <Text size={"1"} style={{marginTop: "5px", opacity: 0.5}} color={"gray"}>Click the &quot;...&quot; to specify a page!</Text>
-        </Grid>
-        <br></br>
-        {(type == "users" ? profiles : nations).map((e: Record<any, any>, i: number) => <Grid style={{placeItems: "center"}} key={e.id}><LeaderboardCard profile={e} nationalities={type == "nations"}></LeaderboardCard><br></br></Grid>)}
-    </Box>
-  )
+            <Text size="5" className="header">This part of the list shows the best {type == "users" ? "players" : "countries"} on the Insane Demon List!</Text>
+            <br></br><br></br>
+            <Grid style={{ placeItems: "center" }}>
+                <br></br>
+                <TextFieldRoot style={{ width: "min(100%, 800px)" }}>
+                    <TextFieldSlot>
+                        <MagnifyingGlassIcon style={{ scale: 1.8, padding: "5px" }}></MagnifyingGlassIcon>
+                    </TextFieldSlot>
+                    <TextFieldInput style={{ fontSize: "20px", height: "40px" }} placeholder='Search...' onChange={async (e) => {
+                        let key = crypto.randomUUID()
+                        queue = key
+                        setTimeout(() => {
+                            if (queue != key) return;
+                            setFilter(e.target.value)
+                            changePage(1, e.target.value, true)
+                        }, 1000)
+                    }}></TextFieldInput>
+                </TextFieldRoot>
+            </Grid>
+            <br></br>
+            <Grid style={{ placeItems: "center" }}>
+                <SelectRoot defaultValue={JSON.stringify(nationality)} onValueChange={(e) => {
+                    let value = JSON.parse(e)
+                    setNationality(value)
+                    changePage(1, undefined, true, value[1])
+                }}>
+                    <SelectTrigger style={{ width: "min(100%, 400px)", fontSize: "20px" }} color="cyan"></SelectTrigger>
+                    <SelectContent>
+                        {Object.entries(cache.nationalities).map(e => <SelectItem key={e[1]} value={JSON.stringify(e)}>
+                            <Flex gap="3">
+                                <Image alt={e[1]} src={e[0] != "International" ? `https://raw.githubusercontent.com/lipis/flag-icons/4f420bdd2e954f6da11220f1136fa181ed7019e7/flags/4x3/${e[1]}.svg` : "/international.png"} width={24} height={18}></Image>
+                                <Text size="3">{e[0]}</Text>
+                            </Flex>
+                        </SelectItem>)}
+                    </SelectContent>
+                </SelectRoot>
+            </Grid>
+            <br></br>
+            <Grid style={{ placeItems: "center" }}>
+                <Button color="cyan" style={{ width: "min(100%, 350px)" }} onClick={async () => {
+                    let t: typeof type = type == "nations" ? "users" : 'nations'
+                    setType(t)
+                    changePage(1, undefined, true, undefined, t)
+                }}>
+                    <Text size="5" as="p" align='center'>Switch to {type == "users" ? "nationalities" : "players"} leaderboard</Text>
+                </Button>
+                <br></br>
+                <Flex gap="2" align='center'>
+                <Text size="3">{type == 'users' ? "Profiles" : "Nationalities"} per page:</Text><TextFieldInput style={{width: "5ch"}} type='number' defaultValue={max} onChange={(e) => {
+                    let value = parseInt(e.target.value)
+                    if(value > 100) {
+                        e.target.value = "100"
+                        value = 100
+                    }
+                    if(value < 10) {
+                        e.target.value = "10"
+                        value = 10
+                    }
+                    let key = crypto.randomUUID()
+                    sliderQueue = key
+                    setTimeout(() => {
+                        if (sliderQueue != key) return;
+                        setMax(value)
+                        changePage(1, undefined, true, undefined, undefined, value)
+                    }, 1000)
+                }}></TextFieldInput>
+                </Flex>
+                <br></br>
+                <Pagination
+                    page={page}
+                    count={pages}
+                    onChange={changePage}
+                ></Pagination>
+                <Text size={"1"} style={{ marginTop: "5px", opacity: 0.5 }} color={"gray"}>Click the &quot;...&quot; to specify a page!</Text>
+            </Grid>
+            <br></br>
+            {(type == "users" ? profiles : nations).map((e: Record<any, any>, i: number) => <Grid style={{ placeItems: "center" }} key={e.id}><LeaderboardCard profile={e} nationalities={type == "nations"}></LeaderboardCard><br></br></Grid>)}
+        </Box>
+    )
 }
