223916c4f18c15ae75ee491060f2991b4c8f1d03
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2026-03-08 15:17:46 -0700

    Update adapter.ts

--- adapter.ts
+++ adapter.ts
@@ -13,4 +13,4 @@ clientPromise = client.connect()
 
 // Export a module-scoped MongoClient promise. By doing this in a
 // separate module, the client can be shared across functions.
-export default clientPromise
+export default clientPromise 
